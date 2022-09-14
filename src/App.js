a0f748004d54807445e1342cbab5e2c06819d3b6
oShadowReaper <94566772+oShadowReaper@users.noreply.github.com>
2022-09-14 15:37:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Eventos from "./components/Eventos/evetos";
 import Jantar from "./components/Jantar/jantar";
 import QuemSomos from "./components/QuemSomos/quemsomos";
 import Salgados from "./components/Salgados/salgado";
-import { FiMenu } from 'react-icons/fi'
+import { FiMenu } from 'react-icons/fi';
+import Admin from "./components/Admin/admin";
+import Cadastro from "./components/CADASTRO/cadastro";
+import Login from "./components/LOGIN/login";
 
 const App = () => {
   return (<div>
@@ -16,12 +19,16 @@ const App = () => {
      <BrowserRouter>
        <Routes>
          <Route element={<Principal />} path={"/"} exact />
+         <Route element={<Doces />} path={"/Doces"} />
          <Route element={<Salgados />} path={"/Salgados"} />
          <Route element={<Almoço />} path={"/Almoco"} />
          <Route element={<Jantar />} path={"/Jantar"} />
          <Route element={<Eventos />} path={"/Eventos"} />
          <Route element={<Comunicados />} path={"/Comunicados"} />
          <Route element={<QuemSomos />} path={"/QuemSomos"} />
+         <Route element={<Admin />} path={"/Admin"} />
+         <Route element={<Cadastro />} path={"/Cadastro"} />
+         <Route element={<Login />} path={"/Login"} />
        </Routes>
      </BrowserRouter>
     </div>
