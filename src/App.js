6059ff32c6cd4740480ee34c819c756afcdf3b16
oShadowReaper <duardogamesyt@gmail.com>
2022-09-22 14:46:53 -0300

    atualizacoes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import Login from "./components/Login/login";
 const App = () => {
   return (<div>
 
-
      <BrowserRouter>
        <Routes>
          <Route element={<Principal />} path={"/"} exact />
