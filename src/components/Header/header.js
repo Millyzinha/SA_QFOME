c890605bbd6198b97ec77b6588a737726758d240
MARCYELEN ROSA DE CARVALHO <marcyelen_carvalho@estudante.sesisenai.org.br>
2022-09-26 17:42:13 -0300

    Mudanças no Carrossel, Footer, Header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,44 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 import { FaBars, FaUserAlt, FaCartPlus } from 'react-icons/fa'
-import './header.css'
-import Logo from './logo2.png'
-
-const Header = () => {
-
-
-    return (
-
-
-        <div className='navbar'>
-            <div className='esquerda'>
-                <FaBars className='menu' />
-                <h1 className='title'>Doces</h1>
-
-            </div>
-            <div className='logo'>
-                <img src={Logo} className='h-logo' />
-            </div>
-            <div className='direita'>
-                < FaUserAlt className='user' />
-                < FaCartPlus className='carrinho' />
-            </div>
-        </div>
-
-
-
-
-
-
-    )
-}
-
-=======
-import React from 'react'
-import Menu from '../Indexhome/menu'
-import { FaBars, FaUserAlt, FaCartPlus } from 'react-icons/fa'
-import './header.css'
-import Logo from './logo2.png'
+import '../Header/header.css'
+import Logo from '../Header/logo2.png'
 
 const Header = () => {
    
@@ -46,10 +9,10 @@ const Header = () => {
     return (
 
         
-        <div className='navbar'>
+        <div className='navbar2'>
             <div className='esquerda'>
                 <FaBars className='menu'/>
-                <h1 className='title'></h1>
+                
                 
                     </div>
                     <div className='logo'>
@@ -69,5 +32,4 @@ const Header = () => {
     )
 }    
 
->>>>>>> bf9ef1f7812dc00fd9ce043cdd29e2b58ad0c322
 export default Header
