0bfac5fa8d05db7f4e44df683c13a6baacaad18c
Marcynha01 <Marcyelen_carvalho@gmail.com>
2022-10-18 17:41:33 -0300

    EDITAR PERFIL E PERFIL

--- src/components/Jantar/jantar.js
+++ src/components/Jantar/jantar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Footer from '../Footer/footer'
+import Header from '../Header/header'
 
 
-const Jantar =() => {
-    return(
+const Jantar = () => {
+    return (
         <div>
             <h2>Jantar</h2>
+            <Header />
+            <Footer />
         </div>
     )
 }
--- src/components/Salgados/salgado.js
+++ src/components/Salgados/salgado.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import Footer from '../Footer/footer'
+import Header from '../Header/header'
 
 const Salgados = () => {
     return (
         <div>
             <h2>Salgados</h2>
-           
+            <Header />
+            <Footer />
+
         </div>
     )
 }
 
 export default Salgados
- 
