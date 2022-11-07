1080b21f42c2d5ad5d489971758214d494ba8f3c
dai222j <rstaffk@gmail.com>
2022-11-07 17:25:30 -0300

    header da home deslogado finalizado

--- src/components/HomeDeslogado/homeDeslogado.js
+++ src/components/HomeDeslogado/homeDeslogado.js
@@ -2,37 +2,36 @@ import React from 'react'
 import Footer from '../Footer/footer'
 import "../HomeDeslogado/homeDeslogado.css"
 import Logo from "../Header/logo.png"
-import { Link } from "react-router-dom"
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 const HOMEDeslogado = () => {
 
-
-    
     return (
     <div className='principal'>
         <div className="header">
-
-            <div className='novidadeslink' >
-        <ul>
-     <Link className='novidades' to={'/novidades'}> Novidades </Link>
-     <Link className='logar' to={'/login'}> Cadastrar </Link>
-     <Link className='cadastrar' to={'/cadastro'}> Logar </Link>
-        </ul>
-
+            <div className='nAVlinkESQUERDA' >
+            <Navbar>
+          <Nav className="me-auto"  >
+            <Nav.Link className='novidades' href="/novidades">NOVIDADES</Nav.Link>
+          </Nav>
+      </Navbar> 
             </div>
-
         <div className='logo'>
                     <img src={Logo} alt="LogoCircular" className='h-logo'/>
                 </div>
-
-
+<div className='navlinkdireita'>
+    <Nav>
+    <Nav.Link className='logar' href="/login">LOGAR</Nav.Link>
+    <Nav.Link className='cadastrar' href="/cadastro">CADASTRAR</Nav.Link>
+    </Nav>
+</div>
         </div>
         <div className='footerdiv'>
             < Footer />
             </div>
-            </div>
-               
+            </div>   
     )
 }    
-
-
 export default HOMEDeslogado 
